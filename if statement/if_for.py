1b47ade4d92a47a5559528eafb8ef3cae799c173
beb777 <bebatbeb@gmail.com>
2021-01-09 22:16:28 +0300

    calender

---
+++ if statement/if_for.py
@@ -0,0 +1,13 @@
+whole_number = [0,1,2,3,4,5,6,7,8,9,10]
+for i in whole_number:
+    if i > 5:
+        print (i)
+    elif  i < 5:
+        print(i*i)
+print('good bye')
+names = ['rob','beb','bob','cal']
+for i in names :
+    if i == 'beb':
+        print(i.upper())
+    else:
+        print(i.title())
