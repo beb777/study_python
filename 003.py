8ee021380007d6f45ad770ae1c20df104a902f3c
beb777 <bebatbeb@gmail.com>
2021-03-09 22:16:32 +0300

    variables

---
+++ 003.py
@@ -0,0 +1,22 @@
+print('------------------------------------------------')
+# variables : assignment, update , new, chained Assignment
+# variables can be numbers or strings
+x = 2 # assigned the value 2 to  a variable x
+x = 6   # updating the value of x from 2 to 6
+x = 'alex'
+print(x)
+y = 'mic'
+print(y)
+a = b = c = 5.6 # chained Assignment
+print(a, b, c)
+d, e = 1, 0 # multiple assignment
+print(d, e)
+y = 'bob'   # new  variable
+print(y)
+# type of variable
+#string(str) , float, integer(int)
+print(type(x))
+print(type(a))
+print(type(d))
+print(type(y))
+print('______________________________')
--- README.md
+++
@@ -1,4 +0,0 @@
-# study_python_online
-- am not the good programmer but one day i can reach that point. 
-try to help me if you are intersted. am ready to learn from you.
-thank you.
