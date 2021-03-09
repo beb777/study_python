151d94a26f4cf21ec35dff49cf323ec8a0972c31
beb777 <bebatbeb@gmail.com>
2021-03-09 14:10:42 +0300

    restart python to study

---
+++ 001.py
@@ -0,0 +1,7 @@
+#python self study with zeraf
+print('|_________________________|')
+print('| welcome to python hub   |')
+print('|                         |')
+print('| python crash course 2021|')
+print('|         2021            |')
+print('|_________________________|')
