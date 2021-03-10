17f7405195c99c584b14174268695ce782fdb76b
beb777 <bebatbeb@gmail.com>
2021-03-10 22:04:55 +0300

    modify

--- 006.py
+++ 006.py
@@ -26,6 +26,10 @@ if x % 2 == 0:
     print('x is divisible by 2')
     print('x is even number')
 
+print('_______________________________________')
+#if else statement
+
+
 print('________________________________________________________')
 
 
