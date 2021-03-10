a14fb37ff60b758059540f85fea7bc054081b1f7
beb777 <bebatbeb@gmail.com>
2021-03-10 10:38:51 +0300

    operator

---
+++ 005.py
@@ -0,0 +1,21 @@
+# oprater
+x = 2
+y = 3
+print('addition: ', x + y)
+print('minus: ', x - y)
+print('divsion: ', x / y)# float result
+print('division: ', y // x)# integer result
+print('modulus: ', y % x)
+print('exponent:', x ** 3)
+print('________________________________________')
+# augmented
+x = x + 2
+print(x)
+x += 2
+print(x)
+y = y - 4
+print(y)
+y -= 8
+print(y)
+y *= 4
+print(y)
