72670ef4162a1076998ebed5d98db829a1d903c7
beb777 <bebatbeb@gmail.com>
2021-03-10 09:24:39 +0300

    modify

--- 004.py
+++ 004.py
@@ -8,4 +8,17 @@ welcome_message = "welcome to python's hub"
 print(welcome_message)
 welcome_message = 'welcome to "python" hun'
 print(welcome_message)
+welcome_message = '''     welcome 
+            to
+             python hub  
+'''
+print(welcome_message)
+# index []
+welcome_message ='welcome to python hubs'
+print(welcome_message[0])# start index
+print(welcome_message[-1])# end index
+print(welcome_message[:])# same as print(welcome_message)
+print(welcome_message[10:17])# printing python 
+
+
 print('******************************************************')
