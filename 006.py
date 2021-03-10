314327c74471d975fefe6a49211e765b69da10c4
beb777 <bebatbeb@gmail.com>
2021-03-10 13:21:12 +0300

    if statemnt

---
+++ 006.py
@@ -0,0 +1,32 @@
+print('___________________________________________________')
+'''
+if condition structure
+condition
+if <condition>:
+    <statement>
+'''
+
+if 4 > 2:
+    print('4 is greater than  2')
+print('_______________________________________________')
+# if condition with logic
+x = 4
+y = 5
+z = 6
+if x < y and x < z:
+    print('x is smallest number')
+# if statement with index
+name = ['bob', 'mark', 'muna']
+if 'mark' in name:
+    print(name[1], ' is on the list')
+print('______________________________________________________')
+# multi statement
+x = 16
+if x % 2 == 0:
+    print('x is divisible by 2')
+    print('x is even number')
+
+print('________________________________________________________')
+
+
+
