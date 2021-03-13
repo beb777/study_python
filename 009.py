f790493cd4ef7b58564f01049d512acb4b1b40ae
beb777 <bebatbeb@gmail.com>
2021-03-13 14:13:01 +0300

    set

---
+++ 009.py
@@ -0,0 +1,18 @@
+print('_______________________________________')
+"""
+set
+-unorder 
+- duplication not allowed
+- union |
+- intersection &
+- difference -
+
+"""
+a = {1, 4, 6, 8, 10}# a set
+b = { 3, 5, 4, 6}
+print(a | b)# union
+print(a & b)# intersection
+print(a - b)# difference
+print('_______________________________________')
+
+print('_______________________________________')
