41b5498626d46b3b18e485ce71b9c738e8b8d76f
beb777 <bebatbeb@gmail.com>
2021-03-13 14:15:12 +0300

    set add differences a in b and b in a

--- 009.py
+++ 009.py
@@ -12,7 +12,8 @@ a = {1, 4, 6, 8, 10}# a set
 b = { 3, 5, 4, 6}
 print(a | b)# union
 print(a & b)# intersection
-print(a - b)# difference
+print(a - b)# difference all in a not in b
+print(b- a)# difference  all in b not in a 
 print('_______________________________________')
 
 print('_______________________________________')
